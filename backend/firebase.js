d1c5f8256cfc6d4bbded3e942ec2ec767ca4d17a
poison <shahsusan291@gmail.com>
2024-12-19 20:49:52 +0545

    init #4

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const express = require('express');
 
-// Initialize Firebase Admin SDK
+
 var serviceAccount = require('./spendy-41ea7-firebase-adminsdk-bb0yz-95988c3e61.json');
 
 admin.initializeApp({
@@ -11,20 +11,20 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// Initialize Express App
+
 const app = express();
 
-// Middleware for parsing JSON body
+
 app.use(bodyParser.json());
 
-// Create a new transaction (form data)
+
 app.post('/api/transaction', async (req, res) => {
   const { userId, title, amount, description, date, category, type, payment } = req.body;
 
-  console.log("Received transaction data:", req.body); // Log incoming data
+  console.log("Received transaction data:", req.body); 
 
   try {
-    // Create a new transaction document in the Firestore "transactions" collection
+    
     const transactionRef = db.collection('transactions').doc();
     await transactionRef.set({
       userId,
@@ -46,7 +46,7 @@ app.post('/api/transaction', async (req, res) => {
   }
 });
 
-// Start server
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });

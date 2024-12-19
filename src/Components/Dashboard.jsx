4ded928fc4f5caf8cadfe00b9184fa7d194b4f2d
poison <shahsusan291@gmail.com>
2024-12-19 16:31:59 +0545

    Changes path

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -4,6 +4,8 @@ export default function Dashboard() {
   return (
    <>
    <div className="bg-gray-700 absolute mx-80 rounded-xl">
+    <h1>This is just a dashboard </h1>
+
     
    </div>
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -16,6 +16,8 @@ export default function Hero() {
     }
   }, [isLoaded, isSignedIn, navigate]);
 
+
+
   return (
     <>
       <Navbar />
